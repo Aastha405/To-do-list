16e59a9c23d13691ea153f0cfc0854b9e3d0ce55
Aastha Kumari <aasthak445@gmail.com>
2025-08-10 13:11:24 +0530

    claim reward function

--- contracts/taskreward.sol
+++ contracts/taskreward.sol
@@ -8,4 +8,18 @@ contract TaskReward {
     function viewEarnedRewards(address _user) external view returns (uint) {
         return earnedRewards[_user];
     }
+
+    // Claim earned rewards and reset balance
+    function claimReward() external {
+        uint reward = earnedRewards[msg.sender];
+        require(reward > 0, "No rewards to claim");
+
+        earnedRewards[msg.sender] = 0;
+
+        // Here we simply send Ether, but this could also be an ERC20 transfer
+        payable(msg.sender).transfer(reward);
+    }
+
+    // Allow contract to receive Ether
+    receive() external payable {}
 }
