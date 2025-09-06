38335b5aa6ede06484026e944b2e58709d9fe7db
Aastha Kumari <aasthak445@gmail.com>
2025-09-06 14:58:11 +0530

    Create 3.0.sol

---
+++ contracts/3.0.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+contract ToDoList {
+    // existing storage and logic...
+
+    // Example: Add new function to return all expired tasks
+    function getExpiredTasks() public view returns (Task[] memory) {
+        // logic to filter and return expired tasks
+    }
+}
